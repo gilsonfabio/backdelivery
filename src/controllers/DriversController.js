728ba5dc2ce5c30d1b9f63296819ff242c52e9cf
Gilson Fabio Silva Modanez <gilson.modanez@aparecida.go.gov.br>
2025-08-28 15:01:40 -0300

    atualização busca motoristas distancia

--- src/controllers/DriversController.js
+++ src/controllers/DriversController.js
@@ -3,24 +3,20 @@ const bcrypt = require('bcrypt');
 const saltRounds = 12;
 require('dotenv/config');
 
-const haversineDistance = (lat1, lon1, lat2, lon2) => {
-    const toRad = (x) => (x * Math.PI) / 180;
-  
-    const R = 6371; 
-    const dLat = toRad(lat2 - lat1);
-    const dLon = toRad(lon2 - lon1);
-  
-    const a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(toRad(lat1)) *
-        Math.cos(toRad(lat2)) *
-        Math.sin(dLon / 2) *
-        Math.sin(dLon / 2);
-  
-    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  
-    return R * c; 
-};
+function haversineDistance(lat1, lon1, lat2, lon2) {
+  const R = 6371e3; // raio da Terra em metros
+  const φ1 = (lat1 * Math.PI) / 180;
+  const φ2 = (lat2 * Math.PI) / 180;
+  const Δφ = ((lat2 - lat1) * Math.PI) / 180;
+  const Δλ = ((lon2 - lon1) * Math.PI) / 180;
+
+  const a =
+    Math.sin(Δφ / 2) ** 2 +
+    Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) ** 2;
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+  return R * c; // distância em metros
+}
 
 module.exports = {       
     async index (request, response) {
